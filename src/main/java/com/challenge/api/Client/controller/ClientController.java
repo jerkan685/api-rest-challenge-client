2aca3fc450d659162aae3fa8352702a0dabb0c95
jerkan <jerkanaf@gmail.com>
2022-09-21 17:08:46 -0300

    agrego documentacion swagger al proyecto y mejoro respuesta

--- src/main/java/com/challenge/api/Client/controller/ClientController.java
+++ src/main/java/com/challenge/api/Client/controller/ClientController.java
@@ -9,9 +9,15 @@ import com.challenge.api.Client.entity.response.DataClientResponse;
 import com.challenge.api.Client.entity.response.KpideClienteResponse;
 import com.challenge.api.Client.interfaces.services.ClientRepository;
 import com.challenge.api.Client.util.MethodShare;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,17 +33,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/api")
 @CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.PUT,RequestMethod.POST})
+@Api(value = "Api challenge", description = "Manejo de datos clientes")
 public class ClientController {
     @Autowired
     private ClientRepository methodDb;
     
     @PostMapping("/creacliente")
-    public Cliente saveClient(@RequestBody Cliente client) {
-     return methodDb.save(client);
+    @ApiOperation(value = "Crea un nuevo cliente",
+            response = DataClientResponse.class)
+    public ResponseEntity<String> saveClient(@RequestBody Cliente client) {
+     String validate = MethodShare.validCliente(client);
+     if(validate != null) {
+       return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body("Error faltan datos de clientes para la creacion");
+     }
+     try {
+         methodDb.save(client);
+         return ResponseEntity.status(HttpStatus.CREATED).body("Cliente "+client.getNombre()+" creado !!!!!!");
+     }catch(Exception e) {
+         return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body("Fallo la creacion del cliente " +  e);   
+     }
     }
     
     @GetMapping("/listClientes")
-    public List<DataClientResponse> getHello() {
+    @ApiOperation(value = "Obtiene informacion de los clientes y la fecha probable de su muerte",
+            response = DataClientResponse.class)
+    public List<DataClientResponse> getlistClient() throws JsonProcessingException, ParseException {
         List<Cliente> clientes = methodDb.getClients();
         List<DataClientResponse> response = new ArrayList<>();
         
@@ -50,7 +70,9 @@ public class ClientController {
     }
     
     @GetMapping("/kpideclientes")
-    public KpideClienteResponse getInfo() {
+    @ApiOperation(value = "Obtenemos la desviacion estandar y el promedio de todas las edades de los clientes",
+            response = KpideClienteResponse.class)
+    public KpideClienteResponse getInfo() throws JsonProcessingException, ParseException {
       return KpideClienteResponse.builder().promedio(methodDb.getPromedy()).desvest(methodDb.getDesvest()).build();
     }
     
--- src/main/java/com/challenge/api/Client/util/MethodShare.java
+++ src/main/java/com/challenge/api/Client/util/MethodShare.java
@@ -3,8 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.challenge.api.Client.util;
+import com.challenge.api.Client.entity.Cliente;
 import java.util.Calendar;
 import java.util.Date;
+import static java.util.Objects.isNull;
 
 /**
  *
@@ -17,6 +19,17 @@ public class MethodShare {
         calendar.setTime(fecha);
         calendar.add(Calendar.YEAR, promedio);
         return calendar.getTime();
+    }
+    
+    public static String validCliente(Cliente client) {
+        if (isNull(client) ||
+                isNull(client.getNombre()) ||
+                isNull(client.getApellido()) ||
+                isNull(client.getEdad()) ||
+                isNull(client.getFechaNacimiento())) {
+           return "datos del cliente incompletos";
+        }
+        return null;
     }
     
 }
